22f5724e7ce8446cd707f90a40b4d750b5723eff
m-pilia <martino.pilia@gmail.com>
2016-01-03 15:17:08 +0100

    bigint parsing, EOI check

--- parser.ml
+++ parser.ml
@@ -2,11 +2,56 @@ open Camlp4.PreCast ;;
 
 module Gram = MakeGram(Lexer) ;;
 
+let exp_eoi = Gram.Entry.mk "exp_eoi";;
 let exp  = Gram.Entry.mk "exp" ;;
 let bind = Gram.Entry.mk "bind";;
 
+
 EXTEND Gram
 
+  (* Ensure that the valid expression is not followed by any trailing 
+   * character.
+   *)
+  exp_eoi: 
+  [ 
+    [ e = exp; EOI -> e ] 
+  ];
+
+  (* BNF syntax for the language.
+   *
+   * <block>      ::= let <bind list> in <exp>
+   * <bind list>  ::= <bind> [and <bind list>]
+   * <bind>       ::= <liden> = <exp>
+   * <int>        ::= [+-]?[0-9]+
+   * <bigint>     ::= Bigint "<int>"
+   * <bool>       ::= true | false
+   * <liden>      ::= [a-z][a-zA-Z]*
+   * <liden list> ::= [<liden> [; <liden list>]]
+   * <arg list>   ::= [<exp> [; <arg list>]]
+   * <cast>       ::= (big) <exp>
+   * <pair>       ::= (<exp>, <exp>)
+   * <list>       ::= [] | <exp> :: <list>
+   * <if>         ::= if <exp> then <exp> else <exp>
+   * <function>   ::= fun (<liden list>) -> <exp>
+   * <apply>      ::= <exp> (<arg list>)
+   *
+   * <exp> ::= <block> | <int> | <bigint> | <bool> | <cast> | <pair> | <list>
+   *         | <liden> | <if> | <fun> | <apply>
+   *         | ( <exp> )
+   *         | <exp> + <exp>
+   *         | <exp> - <exp>
+   *         | <exp> * <exp>
+   *         | <exp> / <exp>
+   *         | <exp> % <exp>
+   *         | <exp> && <exp>
+   *         | <exp> || <exp>
+   *         | ! <exp>
+   *         | ISZERO <exp>
+   *         | FST <exp>
+   *         | SND <exp>
+   *         | HD <exp>
+   *         | TL <exp>
+   *)
   exp: 
   [ 
     "equals" LEFTA
@@ -26,7 +71,7 @@ EXTEND Gram
     [ "(big)"; e = SELF -> Castint(e) ]
   | "Cons" RIGHTA
     [ e1 = exp; "::"; e2 = exp -> Cons(e1,e2)]
-  | "Head,Tail" LEFTA
+  | "Head,Tail" RIGHTA
     [ "HD"; e = exp -> Head(e)
     | "TL"; e = exp -> Tail(e)]
   | "Not" RIGHTA
@@ -39,11 +84,11 @@ EXTEND Gram
   | "Values"
     [ `INT(i,_) -> Eint(i) 
     | `LIDENT x -> Den(x) 
-    | "Bigint"; `INT(i,_) -> Bigint (fst(cast_int i))
+    | "Bigint"; i = STRING -> str_to_bigint i
     | "true"  -> Ebool(true)
     | "false" -> Ebool(false)
     | "("; e = SELF; ")" -> e 
-    | "[]" -> Emptylist
+    | "["; "]" -> Emptylist
     | "("; e1 = exp; ","; e2 = exp; ")" -> Pair(e1,e2)]
   | "Ifthenelse" LEFTA
     [ "if"; e = exp; "then"; c1 = SELF; "else"; c2 = SELF 
--- run.ml
+++ run.ml
@@ -1,11 +1,8 @@
-(* ocamlc -c -I +camlp4 -pp camlp4of.opt main.ml *)
-(* camlp4 main.cmo *)
-
 #use "sem_static.ml";;
 #use "parser.ml";;
 
 let parse_prog s =
-   Gram.parse_string exp (Loc.mk "<string>") s;;
+   Gram.parse_string exp_eoi (Loc.mk "<string>") s;;
 
 let rec fold_lines acc = 
   try 
@@ -15,7 +12,6 @@ let rec fold_lines acc =
 
 let _ = 
   let sprog = fold_lines "" in 
-  (* to run the interpreter *)
   print_string (dump_dval (sem_exp (parse_prog sprog) (emptyenv ())))
 ;;
 
